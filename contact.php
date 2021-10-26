bdfde01fb40e02cf5308e6a8ea46a1c688a6535e
Aholu Samuel <samuelaholu15@gmail.com>
2021-10-26 15:57:50 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -63,8 +63,8 @@ include("data/functions.php");
 	<div class="home">
 		<div class="home_background parallax-window" data-parallax="scroll" data-image-src="images/post.jpg" data-speed="0.8"></div>
 		<div class="home_content">
-			<div class="post_category trans_200"><a href="category.html" class="trans_200">sport</a></div>
-			<div class="post_title">How Did van Gogh’s Turbulent Mind Depict One of the Most Complex Concepts in Physics?</div>
+			
+			<div class="post_title">CONTACT US</div>
 		</div>
 	</div>
 	
