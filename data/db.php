41135bff42af0cf5fbd11190d40d2ff67d9b7da1
Aholu Samuel <samuelaholu15@gmail.com>
2022-03-22 11:10:57 +0100

    change

--- data/db.php
+++ data/db.php
@@ -1,7 +1,7 @@
 <?php
 //creating the database to use
-//$con = mysqli_connect("localhost","certezag_blogger","mycertezapasswordissafe","certezag_data");
-$con = mysqli_connect("localhost","root","","certeza");
+$con = mysqli_connect("localhost","certezag_blogger","mycertezapasswordissafe","certezag_data");
+//$con = mysqli_connect("localhost","root","","certeza");
 
 // function row_count($result) {
 
