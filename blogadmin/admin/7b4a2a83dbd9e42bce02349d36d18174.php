9ee625958efba7014d6b550e49a2f550cf73a7b2
Aholu Samuel <samuelaholu15@gmail.com>
2022-03-22 11:40:45 +0100

    delete

--- blogadmin/admin/7b4a2a83dbd9e42bce02349d36d18174.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$to[] = 'hello@olapearlshub.com';
-	$to[] = 'olamidesenami98@gmail.com';
-	$mailSubject = "A Message to all Users.";
-	$mailMessage = "Something is going on";
-?>
--- blogadmin/libs/db_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-// $con=mysqli_connect("localhost","root","","olapearls_blog");
-$con=mysqli_connect("localhost","certezag_data","certezapasswordsecured","certezag_data");
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
- ?>
