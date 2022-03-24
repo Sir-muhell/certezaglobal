6513b0a5871e0283ec4dc559ca91f28c9334bdb5
Aholu Samuel <samuelaholu15@gmail.com>
2022-03-24 13:26:33 +0100

    Update ajax.js

--- blogger/ajax.js
+++ blogger/ajax.js
@@ -80,7 +80,7 @@ $(document).ready(function () {
       } else {
         $.ajax({
           type: "post",
-          url: "Functions/init.php",
+          url: "functions/init.php",
           data: { username: username, password: password },
           success: function (data) {
             $("#msg").html(data);
