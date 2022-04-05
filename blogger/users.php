47ac8cd3441e1381ae59835822c009875a7bc031
Aholu Samuel <samuelaholu15@gmail.com>
2022-04-05 10:25:54 +0100

    Update users.php

--- blogger/users.php
+++ blogger/users.php
@@ -109,7 +109,7 @@ if(row_count($result) == 0){
                             <?php
                             $user = $row['name'];
                             $id   = $row['id'];
-                            if ($id === '1') { ?>
+                            if ($id === '1' || '15') { ?>
                               
                             <i class="mdi mdi-cancel" style="color: grey;"><input id="username" hidden value="<?php echo $row['name']; ?>"></i>
                             <i class="mdi mdi-delete" style="color: grey;"></i>
