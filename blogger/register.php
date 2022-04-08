9291b1b8ca0e6c249b0f1494e411e1a65f551c97
Aholu Samuel <samuelaholu15@gmail.com>
2022-04-08 18:11:42 +0100

    Update register.php

--- blogger/register.php
+++ blogger/register.php
@@ -7,15 +7,17 @@
 if(!isset($_SESSION['login'])) {
     header("location: ./ ");
 }
+
 $sql = "SELECT * FROM `users` WHERE `user` = '$username' AND `id` = '$id'";
 $result = query($sql);
 $row = fetch_array($result);
+
 if(row_count($result) == 0){
   session_destroy();
   header("location: ./ ");
 }
 
-if ($id !== '1' || '15') {
+if ($id !== '1' || $id !== '15') {
  header("location: ./home?m=<?php echo $id; ?>");
 }
 ?>
