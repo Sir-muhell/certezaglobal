c1085f445c3f957c968373ae32498e0490acb0f6
Aholu Samuel <samuelaholu15@gmail.com>
2022-04-25 09:15:37 +0100

    Update details.php

--- blogger/details.php
+++ blogger/details.php
@@ -13,9 +13,7 @@ if(row_count($result) == 0){
   session_destroy();
   header("location: ./ ");
 }
-if ($id != '1') {
- header("./home?m=<?php echo $id; ?>");
-}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
