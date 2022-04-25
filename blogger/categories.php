35013dce8034e0b3ef709c42247e34d9a535c6df
Aholu Samuel <samuelaholu15@gmail.com>
2022-04-25 10:17:35 +0100

    Update categories.php

--- blogger/categories.php
+++ blogger/categories.php
@@ -15,10 +15,7 @@ if(row_count($result) == 0){
   header("location: ./ ");
 }
 
-if ($id !== '1') {
-  if ($id !== '15') {
-   header("location: ./home?m=<?php echo $id; ?>");
-  }
+if ($id > 2) {
  header("location: ./home?m=<?php echo $id; ?>");
 }
 
