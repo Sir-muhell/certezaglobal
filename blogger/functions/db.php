9ceaa34b13e2d3d25252e4517146ebc52a4d0773
Aholu Samuel <samuelaholu15@gmail.com>
2022-07-06 21:54:18 +0100

    Publishing

--- blogger/functions/db.php
+++ blogger/functions/db.php
@@ -1,8 +1,9 @@
 <?php
 //creating the database to use
 // $con = mysqli_connect("localhost","root","","certeza");
- $con = mysqli_connect("localhost","root","","certeza");
-//$con = mysqli_connect("localhost","certezag_blogger","mycertezapasswordissafe","certezag_data");
+
+// $con = mysqli_connect("localhost","root","","certeza");
+$con = mysqli_connect("localhost","certezag_blogger","mycertezapasswordissafe","certezag_data");
 
 
 ?>
